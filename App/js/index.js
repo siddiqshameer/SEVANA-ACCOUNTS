b5a06f625783c5e457185107212e5eef484fa707
siddiq shameer <siddiqshameer@outlook.com>
2023-08-15 11:06:41 +0530

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,61 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"xui_ui_comboinput5")
+                .setLeft("0em")
+                .setTop("1.5238095238095237em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Counter")
+                .setType("dropbutton")
+                .setItems([
+                    {
+                        "id" : "1",
+                        "caption" : "1"
+                    },
+                    {
+                        "id" : "2",
+                        "caption" : "2"
+                    },
+                    {
+                        "id" : "3",
+                        "caption" : "3"
+                    },
+                    {
+                        "id" : "4",
+                        "caption" : "4"
+                    },
+                    {
+                        "id" : "5",
+                        "caption" : "5"
+                    },
+                    {
+                        "id" : "6",
+                        "caption" : "6"
+                    },
+                    {
+                        "id" : "7",
+                        "caption" : "7"
+                    },
+                    {
+                        "id" : "8",
+                        "caption" : "8"
+                    },
+                    {
+                        "id" : "9",
+                        "caption" : "9"
+                    },
+                    {
+                        "id" : "10",
+                        "caption" : "10"
+                    }
+                ])
+                .setValue("1")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
